86e414554d7ae2cc446b8b8d066472a66b9bd6ca
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2026-02-26 23:09:53 -0300

    feat: structures
    define a block with some metadatas and functions

---
+++ internal/structures/block.go
@@ -0,0 +1,43 @@
+package structures
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Index     int
+	Timestamp time.Time
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+func (b *Block) CalculateHash() string {
+	data := strconv.Itoa(b.Index) + b.Timestamp.Format(time.RFC3339) + strconv.Itoa(b.BPM) + b.PrevHash
+	hash := sha256.Sum256([]byte(data))
+
+	return hex.EncodeToString(hash[:])
+}
+
+func NewBlock(index int, BPM int, prevHash string) *Block {
+	block := &Block{
+		Index:     index,
+		Timestamp: time.Now(),
+		BPM:       BPM,
+		PrevHash:  prevHash,
+	}
+
+	block.Hash = block.CalculateHash()
+	return block
+}
+
+func GenerateGenesisBlock() *Block {
+	return NewBlock(0, 0, "0")
+}
+
+func (b *Block) IsValid() bool {
+	return b.Hash == b.CalculateHash()
+}
